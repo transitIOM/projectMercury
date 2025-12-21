33af929f8274111c330b5f849cb4ebb44b1c7273
Jayden Thompson <jaydenthompson08@protonmail.ch>
2025-12-21 22:53:44 +0000

    reverted accidental line deletion

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,11 +6,13 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	log "github.com/sirupsen/logrus"
+	"github.com/transitIOM/projectMercury/internal/handlers"
 )
 
 func main() {
 	log.SetReportCaller(true)
 	r := chi.NewRouter()
+	handlers.Handler(r)
 
 	fmt.Println("Starting transit-IOMAPI service...")
 
