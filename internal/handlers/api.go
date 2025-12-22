47710e0ceba4eff9420ab91ee5dd4d07cd197477
Jayden Thompson <jaydenthompson08@protonmail.ch>
2025-12-22 00:05:37 +0000

    removed dead jwt code

--- internal/handlers/api.go
+++ internal/handlers/api.go
@@ -3,7 +3,6 @@ package handlers
 import (
 	"fmt"
 	"net/http"
-	"os"
 	"time"
 
 	"github.com/go-chi/chi/v5"
@@ -15,7 +14,6 @@ import (
 var tokenAuth *jwtauth.JWTAuth
 
 func Handler(r *chi.Mux) {
-	tokenAuth = jwtauth.New("HS256", []byte(os.Getenv("JWT_KEY")), nil)
 	r.Use(middleware.RequestID)
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
@@ -32,18 +30,13 @@ func Handler(r *chi.Mux) {
 		r.Group(func(r chi.Router) {
 			r.Get("/version", GetGTFSScheduleVersionID)
 			r.Get("/", GetGTFSScheduleDownloadURL)
-			r.Get("/authenticate", GetAdminToken)
 		})
 
 		// private routes
 		r.Group(func(r chi.Router) {
-			// verify token
-			r.Use(jwtauth.Verifier(tokenAuth))
-			r.Use(jwtauth.Authenticator(tokenAuth))
 			r.Put("/", PutGTFSSchedule)
 			r.Get("/admin", func(w http.ResponseWriter, req *http.Request) {
-				_, claims, _ := jwtauth.FromContext(req.Context())
-				w.Write([]byte(fmt.Sprintf("protected area. hi %v", claims["user_id"])))
+				w.Write([]byte(fmt.Sprintf("protected area.")))
 			})
 		})
 	})
