d6a177d92f26ad16439e5d97adcde739e9c89d9f
Jayden Thompson <jaydenthompson08@protonmail.ch>
2025-12-29 23:47:41 +0000

    Fix missing status code in `GetMessagesResponse`: add `WriteHeader` call to ensure proper HTTP response status

--- internal/handlers/getMessages.go
+++ internal/handlers/getMessages.go
@@ -48,6 +48,7 @@ func GetMessages(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
+	w.WriteHeader(response.Code)
 	w.Header().Set("Content-Type", "application/json")
 	err = json.NewEncoder(w).Encode(response)
 	if err != nil {
