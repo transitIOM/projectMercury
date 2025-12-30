ff8cc4c8521f96011bc2af695c268e716164c6b1
Jayden Thompson <jaydenthompson08@protonmail.ch>
2025-12-30 00:38:26 +0000

    Remove duplicate `json.NewEncoder` call in `getMessages` handler

--- internal/handlers/getMessages.go
+++ internal/handlers/getMessages.go
@@ -51,7 +51,6 @@ func GetMessages(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(response.Code)
 	err = json.NewEncoder(w).Encode(response)
-	err = json.NewEncoder(w).Encode(response)
 	if err != nil {
 		log.Error(err)
 		api.InternalErrorHandler(w)
