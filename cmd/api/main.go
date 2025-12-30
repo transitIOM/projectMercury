53f225c922646b046ce42dab56764a2b5ac8709c
Jayden Thompson <jaydenthompson08@protonmail.ch>
2025-12-30 16:08:14 +0000

    Add graceful shutdown to HTTP server using context and signal handling

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/joho/godotenv"
@@ -26,10 +30,35 @@ func main() {
 	r := chi.NewRouter()
 	handlers.Handler(r)
 
-	fmt.Println("Starting transit-IOMAPI service...")
-
-	err := http.ListenAndServe(":8090", r)
-	if err != nil {
-		log.Error(err)
+	srv := &http.Server{
+		Addr:    ":8090",
+		Handler: r,
 	}
+
+	go func() {
+		fmt.Println("Starting transit-IOMAPI service...")
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server with
+	// a timeout of 5 seconds.
+	quit := make(chan os.Signal, 1)
+	// kill (no param) default send syscall.SIGTERM
+	// kill -2 is syscall.SIGINT
+	// kill -9 is syscall.SIGKILL but can't be caught, so no need to add it
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Info("Shutting down server...")
+
+	// The context is used to inform the server it has 5 seconds to finish
+	// the request it is currently handling
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown: ", err)
+	}
+
+	log.Info("Server exiting")
 }
