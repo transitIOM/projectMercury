1244cd9c4579bbf50712d79a46643285825b9eaa
Jayden Thompson <jaydenthompson08@protonmail.ch>
2026-01-03 13:57:35 +0000

    removed bearer from auth string, replaced teamID and tags with UUIDs

--- internal/tools/linear.go
+++ internal/tools/linear.go
@@ -19,10 +19,8 @@ func InitialiseLinearGraphqlConnection() {
 	}
 
 	client = graphql.NewClient("https://api.linear.app/graphql", nil)
-
-	authHeader := fmt.Sprintf("Bearer %s", linearAPIKey)
 	client = client.WithRequestModifier(func(req *http.Request) {
-		req.Header.Set("Authorization", authHeader)
+		req.Header.Set("Authorization", linearAPIKey)
 		req.Header.Set("Content-Type", "application/json")
 	})
 }
@@ -48,32 +46,49 @@ type IssueCreateInput struct {
 	Priority    int      `json:"priority,omitempty"`
 }
 
+var tagUUIDMap = map[string]string{
+	"bug":         "9536247d-eaca-4319-8fb0-5667037a828e",
+	"user-report": "167f2203-ab68-4ff0-8a82-a12293d49b01",
+	"realtime":    "85a1d688-58de-4208-87ee-b6ac4907fdc2",
+	"schedule":    "a0e52de7-19a5-4c27-9d4a-898afdb1645f",
+}
+
 func CreateIssueFromReport(ctx context.Context, title, description, email string, tags []string) error {
 	var mutation IssueCreateMutation
-	teamID := "DEV"
+	teamID := "b3a913ce-ea8c-4d0f-8c9a-15630b40ea80"
 
 	description = fmt.Sprintf("Submitted by: %s\n\n%s", email, description)
+
+	var tagIds []string
+	for _, tag := range tags {
+		tagIds = append(tagIds, tagUUIDMap[tag])
+	}
 
 	variables := map[string]interface{}{
 		"input": IssueCreateInput{
 			Title:       title,
 			Description: description,
 			TeamID:      teamID,
-			Labels:      tags,
+			Labels:      tagIds,
 			Priority:    0, // 0=None, 1=Urgent, 2=High, 3=Normal, 4=Low
 		},
 	}
 
 	err := client.Mutate(ctx, &mutation, variables)
 	if err != nil {
+		log.WithError(err).Error("Linear mutation failed")
 		return fmt.Errorf("failed to create issue: %w", err)
 	}
+
+	log.WithFields(log.Fields{
+		"mutation": mutation,
+	}).Debug("Linear mutation response")
 
 	if !mutation.IssueCreate.Success {
 		return fmt.Errorf("issue creation failed")
 	}
 
-	log.Debug("Created issue: %s (%s)",
+	log.Infof("Created issue: %s (%s)",
 		mutation.IssueCreate.Issue.Identifier,
 		mutation.IssueCreate.Issue.URL)
 
