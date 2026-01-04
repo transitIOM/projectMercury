18e401581dad6678a47bc861c9aa00331c7d7dfa
Jayden Thompson <jaydenthompson08@protonmail.ch>
2026-01-04 21:34:36 +0000

    Add `/locations` endpoint to expose real-time bus GPS data via API and update documentation. Also fixed signalr processing and now background monitor rather than intercept responses.

--- internal/handlers/getBusLocations.go
+++ internal/handlers/getBusLocations.go
@@ -10,23 +10,43 @@ import (
 	"github.com/transitIOM/projectMercury/internal/tools"
 )
 
+// GetBusLocations godoc
+// @Summary      Get current bus locations
+// @Description  Retrieves real-time GPS locations for all active buses.
+// @Tags         locations
+// @Produce      json
+// @Success      200  {object}  api.GetBusLocationsResponse
+// @Failure      500  {object}  api.Error
+// @Router       /locations/ [get]
 func GetBusLocations(w http.ResponseWriter, r *http.Request) {
 	log.Debug("Handling getBusLocations request")
 	tools.BusLocations.Mutex.RLock()
-	defer tools.BusLocations.Mutex.RUnlock()
-	busLocations := tools.BusLocations.Data
-	nullifyObsoleteValues(&busLocations)
+	memBusLocations := tools.BusLocations.Data
+	tools.BusLocations.Mutex.RUnlock()
+	nullifyObsoleteValues(&memBusLocations)
 
-	busLocationsBytes, err := json.Marshal(busLocations)
+	busLocationsBytes, err := json.Marshal(memBusLocations)
 	if err != nil {
 		log.Error(err)
 		api.InternalErrorHandler(w)
 		return
 	}
 
+	stringBusLocations := string(busLocationsBytes)
+
+	response := api.GetBusLocationsResponse{
+		Code:      http.StatusOK,
+		Locations: stringBusLocations,
+	}
+
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	w.Write(busLocationsBytes)
+	err = json.NewEncoder(w).Encode(response)
+	if err != nil {
+		log.Error(err)
+		api.InternalErrorHandler(w)
+		return
+	}
 }
 
 func nullifyObsoleteValues(busLocations *[]tools.BusLocation) {
