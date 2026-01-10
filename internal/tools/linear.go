e1e4c0bd7201ff3fa09f8006a9d083522244c1c3
Jayden Thompson <jaydenthompson08@protonmail.ch>
2026-01-10 23:44:09 +0000

    Revert "removed bearer from auth string, replaced teamID and tags with UUIDs"
    This reverts commit a738570e74262265d360f9acdd8edf1d987bf421.

--- internal/tools/linear.go
+++ internal/tools/linear.go
@@ -19,8 +19,10 @@ func InitialiseLinearGraphqlConnection() {
 	}
 
 	client = graphql.NewClient("https://api.linear.app/graphql", nil)
+
+	authHeader := fmt.Sprintf("Bearer %s", linearAPIKey)
 	client = client.WithRequestModifier(func(req *http.Request) {
-		req.Header.Set("Authorization", linearAPIKey)
+		req.Header.Set("Authorization", authHeader)
 		req.Header.Set("Content-Type", "application/json")
 	})
 }
@@ -46,49 +48,32 @@ type IssueCreateInput struct {
 	Priority    int      `json:"priority,omitempty"`
 }
 
-var tagUUIDMap = map[string]string{
-	"bug":         "9536247d-eaca-4319-8fb0-5667037a828e",
-	"user-report": "167f2203-ab68-4ff0-8a82-a12293d49b01",
-	"realtime":    "85a1d688-58de-4208-87ee-b6ac4907fdc2",
-	"schedule":    "a0e52de7-19a5-4c27-9d4a-898afdb1645f",
-}
-
 func CreateIssueFromReport(ctx context.Context, title, description, email string, tags []string) error {
 	var mutation IssueCreateMutation
-	teamID := "b3a913ce-ea8c-4d0f-8c9a-15630b40ea80"
+	teamID := "DEV"
 
 	description = fmt.Sprintf("Submitted by: %s\n\n%s", email, description)
-
-	var tagIds []string
-	for _, tag := range tags {
-		tagIds = append(tagIds, tagUUIDMap[tag])
-	}
 
 	variables := map[string]interface{}{
 		"input": IssueCreateInput{
 			Title:       title,
 			Description: description,
 			TeamID:      teamID,
-			Labels:      tagIds,
+			Labels:      tags,
 			Priority:    0, // 0=None, 1=Urgent, 2=High, 3=Normal, 4=Low
 		},
 	}
 
 	err := client.Mutate(ctx, &mutation, variables)
 	if err != nil {
-		log.WithError(err).Error("Linear mutation failed")
 		return fmt.Errorf("failed to create issue: %w", err)
 	}
-
-	log.WithFields(log.Fields{
-		"mutation": mutation,
-	}).Debug("Linear mutation response")
 
 	if !mutation.IssueCreate.Success {
 		return fmt.Errorf("issue creation failed")
 	}
 
-	log.Infof("Created issue: %s (%s)",
+	log.Debug("Created issue: %s (%s)",
 		mutation.IssueCreate.Issue.Identifier,
 		mutation.IssueCreate.Issue.URL)
 
